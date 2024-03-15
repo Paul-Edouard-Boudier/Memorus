61c7fe6ebc1573b72411e9b48b7f6fa21cc4f541
Paul-Edouard <paul.edouard.boudier@gmail.com>
2024-03-15 14:47:17 +0100

    Ajout de toutes les modifs, je n'ai pas bien versionné mais on à une apli fonctionnelle, il ne manque plus que le Delete et voir comment gérer les memoroj si le tag disparaît.

---
+++ Memorus/Utils/Uitls.swift
@@ -0,0 +1,8 @@
+//
+//  Uitls.swift
+//  Memorus
+//
+//  Created by Paul-Edouard Boudier on 10/03/2024.
+//
+
+import Foundation
---
+++ Memorus/Views/EditTagView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTagView.swift
+//  Memorus
+//
+//  Created by Paul-Edouard Boudier on 12/03/2024.
+//
+
+import SwiftUI
+
+struct EditTagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTagView()
+}
